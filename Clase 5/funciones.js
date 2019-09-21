f6ab63338cf3e1f25965ef06fdcf83d4e8284220
Kenji Ushiro <kenjiushiro@live.com>
2019-09-20 21:14:24 -0300

    asd

--- Clase 5/funciones.js
+++ Clase 5/funciones.js
@@ -13,14 +13,32 @@ function EnviarClicked(e){
     var xhttp = new XMLHttpRequest();
     var user = document.getElementById("txtUsername").value;
     var password = document.getElementById("txtPassword").value;
+
+    var loadingIcon = document.getElementById("loading-icon");
+
+    //loadingIcon.hidden= false;
+    loadingIcon.style.visibility = 'visible';
     
     xhttp.onreadystatechange = function(){
         if (xhttp.readyState === 4 && xhttp.status === 200  )
-            console.log("Tenemos respuesta",xhttp.responseText);
+        console.log("Tenemos respuesta",xhttp.responseText);
+            loadingIcon.style.visibility = 'hidden';
+            loadingIcon.hidden=true;
     }
+    //GET
+    /*
     xhttp.open("GET","http://localhost:3000/loginUsuario?usr=" + user + "&pass=" + password,true);
     xhttp.send();
+    */
+
+    //POST
+    xhttp.open("POST","http://localhost:3000/loginUsuario");
+    //El setRequestHeder setea el tipo de valor que se le va a pasar y el formato
+    xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+    xhttp.send("usr=" + user + "&pass=" + password);
 }
+
+
 
 function demo1()
 {
